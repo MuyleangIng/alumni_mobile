3d8b7a34da7b9bbc216f37b7d68c49114b245fad
Muyleang Ing <ing.muyleang2019@rupp.edu.kh>
2024-06-12 17:53:13 +0700

    update

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,9 +1,38 @@
+import 'package:Alumni/views/home/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomeScreen(),
   ));
+}
+
+class Post {
+  final String name;
+  final String status;
+  final String image;
+  int likes;
+  List<Comment> comments;
+
+  Post({
+    required this.name,
+    required this.status,
+    required this.image,
+    this.likes = 0,
+    List<Comment>? comments,
+  }) : comments = comments ?? [];
+}
+
+class Comment {
+  final String user;
+  final String text;
+  List<Comment> replies;
+
+  Comment({
+    required this.user,
+    required this.text,
+    List<Comment>? replies,
+  }) : replies = replies ?? [];
 }
 
 class HomeScreen extends StatefulWidget {
@@ -14,56 +43,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<Map<String, dynamic>> cardsData = [
-    {
-      'name': 'MuyleangIng',
-      'status': 'Status message or description here',
-      'image': 'assets/images/2.jpg',
-      'likes': 10,
-      'comments': 5,
-    },
-    {
-      'name': 'John Doe',
-      'status': 'Another status message here',
-      'image': 'assets/images/rith.jpg',
-      'likes': 20,
-      'comments': 2,
-    },
-    {
-      'name': 'Jane Smith',
-      'status': 'Yet another status message here',
-      'image': 'assets/images/2.jpg',
-      'likes': 5,
-      'comments': 1,
-    },
-    {
-      'name': 'Alex Johnson',
-      'status': 'This is a different status message',
-      'image': 'assets/images/1.jpg',
-      'likes': 15,
-      'comments': 3,
-    },
+  final List<Post> posts = [
+    Post(
+      name: 'MuyleangIng',
+      status: 'Status message or description here',
+      image: 'assets/images/2.jpg',
+      likes: 10,
+      comments: [
+        Comment(
+          user: 'Jane Doe',
+          text: 'Nice post!',
+          replies: [
+            Comment(user: 'John Doe', text: 'I agree!'),
+          ],
+        ),
+        Comment(
+          user: 'Sam Smith',
+          text: 'Great job!',
+        ),
+      ],
+    ),
+    Post(
+      name: 'John Doe',
+      status: 'Another status message here',
+      image: 'assets/images/rith.jpg',
+      likes: 20,
+      comments: [],
+    ),
+    Post(
+      name: 'Jane Smith',
+      status: 'Yet another status message here',
+      image: 'assets/images/2.jpg',
+      likes: 5,
+      comments: [],
+    ),
+    Post(
+      name: 'Alex Johnson',
+      status: 'This is a different status message',
+      image: 'assets/images/1.jpg',
+      likes: 15,
+      comments: [],
+    ),
   ];
 
   void _likePost(int index) {
     setState(() {
-      cardsData[index]['likes']++;
+      posts[index].likes++;
     });
   }
 
-  void _commentOnPost(int index, String comment) {
+  void _commentOnPost(int postIndex, String comment) {
     setState(() {
-      cardsData[index]['comments']++;
-      // Here you can also store the comment if needed
+      posts[postIndex].comments.add(Comment(user: 'Current User', text: comment));
     });
   }
 
-  void _sharePost(int index) {
-    // Implement share functionality here
-    print('Post shared');
+  void _replyToComment(int postIndex, int commentIndex, String reply) {
+    setState(() {
+      posts[postIndex].comments[commentIndex].replies.add(Comment(user: 'Current User', text: reply));
+    });
   }
 
-  void _showCommentDialog(BuildContext context, int index) {
+  void _showCommentDialog(BuildContext context, int postIndex) {
     final TextEditingController commentController = TextEditingController();
     showDialog(
       context: context,
@@ -83,10 +124,41 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             TextButton(
               onPressed: () {
-                _commentOnPost(index, commentController.text);
+                _commentOnPost(postIndex, commentController.text);
                 Navigator.of(context).pop();
               },
               child: Text('Post'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showReplyDialog(BuildContext context, int postIndex, int commentIndex) {
+    final TextEditingController replyController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Reply to Comment'),
+          content: TextField(
+            controller: replyController,
+            decoration: InputDecoration(hintText: 'Enter your reply'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _replyToComment(postIndex, commentIndex, replyController.text);
+                Navigator.of(context).pop();
+              },
+              child: Text('Reply'),
             ),
           ],
         );
@@ -105,8 +177,8 @@ class _HomeScreenState extends State<HomeScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Image.asset(
-                  'assets/images/alumni-text.jpg', // Path to your local image
-                  height: 30, // Adjust the height to fit your design
+                  'assets/images/alumni-text.jpg',
+                  height: 30,
                 ),
               ],
             ),
@@ -130,7 +202,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   style: TextStyle(color: Colors.black),
                   onSubmitted: (query) {
-                    // Add your search logic here
                     print('Search query: $query');
                   },
                 ),
@@ -144,9 +215,9 @@ class _HomeScreenState extends State<HomeScreen> {
               onPressed: () {},
               icon: CircleAvatar(
                 backgroundImage: AssetImage('assets/images/1.jpg'),
-                radius: 20, // Set the radius to adjust the size of the CircleAvatar
+                radius: 20,
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -156,68 +227,15 @@ class _HomeScreenState extends State<HomeScreen> {
           SliverList(
             delegate: SliverChildBuilderDelegate(
                   (BuildContext context, int index) {
-                final card = cardsData[index];
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    elevation: 4.0,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ListTile(
-                            leading: CircleAvatar(
-                              backgroundImage: AssetImage('assets/icon/app_icon.png'),
-                              radius: 25,
-                            ),
-                            title: Text(card['name']!),
-                            subtitle: Text(card['status']!),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            'This is a detailed description that can span multiple lines and gives more information about the status.',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          SizedBox(height: 10),
-                          Image.asset(card['image']!),
-                          SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              TextButton.icon(
-                                onPressed: () => _likePost(index),
-                                icon: Icon(Icons.thumb_up),
-                                label: Text('Like'),
-                              ),
-                              TextButton.icon(
-                                onPressed: () {
-                                  _showCommentDialog(context, index);
-                                },
-                                icon: Icon(Icons.comment),
-                                label: Text('Comment'),
-                              ),
-                              TextButton.icon(
-                                onPressed: () => _sharePost(index),
-                                icon: Icon(Icons.share),
-                                label: Text('Share'),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('${card['likes']} Likes'),
-                              Text('${card['comments']} Comments'),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                final post = posts[index];
+                return PostCard(
+                  post: post,
+                  onLike: () => _likePost(index),
+                  onComment: () => _showCommentDialog(context, index),
+                  onReply: (commentIndex) => _showReplyDialog(context, index, commentIndex),
                 );
               },
-              childCount: cardsData.length,
+              childCount: posts.length,
             ),
           ),
         ],
@@ -234,6 +252,182 @@ class _HomeScreenState extends State<HomeScreen> {
           BottomNavigationBarItem(icon: Icon(Icons.event, color: Colors.black), label: 'Events'),
           BottomNavigationBarItem(icon: Icon(Icons.work, color: Colors.black), label: 'Job'),
         ],
+      ),
+    );
+  }
+}
+
+class PostCard extends StatefulWidget {
+  final Post post;
+  final VoidCallback onLike;
+  final VoidCallback onComment;
+  final Function(int) onReply;
+
+  const PostCard({
+    Key? key,
+    required this.post,
+    required this.onLike,
+    required this.onComment,
+    required this.onReply,
+  }) : super(key: key);
+
+  @override
+  _PostCardState createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  int _visibleComments = 1;
+  Map<int, int> _visibleReplies = {};
+
+  void _showMoreComments() {
+    setState(() {
+      _visibleComments += 1;
+    });
+  }
+
+  void _showMoreReplies(int commentIndex) {
+    setState(() {
+      _visibleReplies[commentIndex] = (_visibleReplies[commentIndex] ?? 1) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 4.0,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/icon/app_icon.png'),
+                  radius: 25,
+                ),
+                title: Text(widget.post.name),
+                subtitle: Text(widget.post.status),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'This is a detailed description that can span multiple lines and gives more information about the status.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Image.asset(widget.post.image),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Text(
+                      '${widget.post.likes} Likes',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  TextButton.icon(
+                    onPressed: widget.onLike,
+                    icon: Icon(Icons.thumb_up),
+                    label: Text('Like'),
+                  ),
+                  TextButton.icon(
+                    onPressed: widget.onComment,
+                    icon: Icon(Icons.comment),
+                    label: Text('Comment'),
+                  ),
+                  TextButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.share),
+                    label: Text('Share'),
+                  ),
+                ],
+              ),
+              Text(
+                'Comments (${widget.post.comments.length})',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              for (var i = 0;
+              i <
+                  (_visibleComments < widget.post.comments.length
+                      ? _visibleComments
+                      : widget.post.comments.length);
+              i++)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 15,
+                          backgroundImage:
+                          AssetImage('assets/icon/app_icon.png'),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          widget.post.comments[i].user,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Text(widget.post.comments[i].text),
+                        ),
+                        if (widget.post.comments[i].replies.isNotEmpty)
+                          TextButton(
+                            onPressed: () => _showMoreReplies(i),
+                            child: Text('See Replies'),
+                          ),
+                      ],
+                    ),
+                    if (_visibleReplies[i] != null)
+                      for (var j = 0;
+                      j <
+                          (_visibleReplies[i]! <
+                              widget.post.comments[i].replies.length
+                              ? _visibleReplies[i]!
+                              : widget.post.comments[i].replies.length);
+                      j++)
+                        Padding(
+                          padding: const EdgeInsets.only(left: 32.0),
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 15,
+                                backgroundImage:
+                                AssetImage('assets/icon/app_icon.png'),
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                widget.post.comments[i].replies[j].user,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(width: 10),
+                              Expanded(
+                                child: Text(
+                                    widget.post.comments[i].replies[j].text),
+                              ),
+                            ],
+                          ),
+                        ),
+                    if (_visibleReplies[i] != null &&
+                        _visibleReplies[i]! <
+                            widget.post.comments[i].replies.length)
+                      TextButton(
+                        onPressed: () => _showMoreReplies(i),
+                        child: Text('See More Replies'),
+                      ),
+                  ],
+                ),
+              if (_visibleComments < widget.post.comments.length)
+                TextButton(
+                  onPressed: _showMoreComments,
+                  child: Text('See More Comments'),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
